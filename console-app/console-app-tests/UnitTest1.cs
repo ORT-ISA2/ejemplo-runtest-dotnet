fcfc22f796aaadf50739a89a2863552fca3c2a05
cpfontan <carina.fontan@fi365.ort.edu.uy>
2022-05-08 17:25:54 -0300

    Add console-app project

---
+++ console-app/console-app-tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace console_app_tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int testNumber = 10;
+
+            Assert.AreEqual(10, testNumber);
+        }
+    }
+}
---
+++ console-app/console-app/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace console_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejemplo ORT - ISA 2");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
